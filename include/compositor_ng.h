966ef6e124e457e4df0dd7f013f5ec05f15c8e1a
Hannes Winkler <hanneswinkler2000@web.de>
2022-02-23 15:00:17 +0000

    add compositor ng skeleton

---
+++ include/compositor_ng.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Compositor NG
+ *
+ * - newer flutter compositor
+ *
+ * Copyright (c) 2022, Hannes Winkler <hanneswinkler2000@web.de>
+ */
+
+
+#ifndef _FLUTTERPI_INCLUDE_COMPOSITOR_NG_H
+#define _FLUTTERPI_INCLUDE_COMPOSITOR_NG_H
+
+struct compositor;
+
+#endif // _FLUTTERPI_INCLUDE_COMPOSITOR_NG_H
