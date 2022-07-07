9db44ab29be172784c501f95a5bb18775d11e9ca
Hannes Winkler <hanneswinkler2000@web.de>
2022-07-07 21:03:42 +0100

    fixes - fix resource uploading context not being made current - fix flutterpi_send_platform_message not calling "runs_platform_tasks_on_current_thread" correctly

--- src/flutter-pi.c
+++ src/flutter-pi.c
@@ -255,7 +255,7 @@ static bool on_make_resource_current(void *userdata) {
     flutterpi = userdata;
     DEBUG_ASSERT_NOT_NULL(flutterpi->gl_renderer);
     
-    ok = gl_renderer_clear_current(flutterpi->gl_renderer);
+    ok = gl_renderer_make_flutter_resource_uploading_context_current(flutterpi->gl_renderer);
     if (ok != 0) {
         return false;
     }
@@ -742,6 +742,7 @@ static int on_send_platform_message(
 }
 
 int flutterpi_send_platform_message(
+    struct flutterpi *flutterpi,
     const char *channel,
     const uint8_t *restrict message,
     size_t message_size,
@@ -751,9 +752,9 @@ int flutterpi_send_platform_message(
     FlutterEngineResult result;
     int ok;
     
-    if (runs_platform_tasks_on_current_thread(NULL)) {
-        result = flutterpi.flutter.procs.SendPlatformMessage(
-            flutterpi.flutter.engine,
+    if (runs_platform_tasks_on_current_thread(flutterpi)) {
+        result = flutterpi->flutter.procs.SendPlatformMessage(
+            flutterpi->flutter.engine,
             &(const FlutterPlatformMessage) {
                 .struct_size = sizeof(FlutterPlatformMessage),
                 .channel = channel,
