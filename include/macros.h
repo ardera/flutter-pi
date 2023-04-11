380f8455ea7c3f2039220f9126ed1e0bb908d064
Hannes Winkler <hanneswinkler2000@web.de>
2023-04-11 23:03:04 +0100

    implement fallback for `__builtin_popcount`

--- include/macros.h
+++ include/macros.h
@@ -583,7 +583,20 @@ typedef int lock_cap_t;
 #ifdef HAVE___BUILTIN_POPCOUNT
     #define HWEIGHT(x) __builtin_popcount(x)
 #else
-    #define HWEIGHT(x) UNIMPLEMENTED()
+static inline int hamming_weight(uint64_t x) {
+    //put count of each 2 bits into those 2 bits
+    x -= (x >> 1) & 0x5555555555555555;
+    
+    // put count of each 4 bits into those 4 bits
+    x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333);  
+    
+    // put count of each 8 bits into those 8 bits
+    x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f;
+
+    // returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...         
+    return (x * 0x0101010101010101) >> 56;   
+}
+    #define HWEIGHT(x) hamming_weight(x)
 #endif
 
 #endif /* UTIL_MACROS_H */
