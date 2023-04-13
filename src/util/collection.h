aa895fe3d8fc6a1845f9481d1796ab930ccf8b2b
Hannes Winkler <hanneswinkler2000@web.de>
2023-04-13 18:52:21 +0200

    apply refactors to `src/util/collection.h`

--- src/util/collection.h
+++ src/util/collection.h
@@ -7,7 +7,6 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -17,7 +16,11 @@
 #include <pthread.h>
 
 #include <features.h>
-#include <macros.h>
+
+#include "geometry.h"
+#include "macros.h"
+#include "refcounting.h"
+#include "uuid.h"
 
 struct queue {
     size_t start_index;
@@ -67,8 +70,6 @@ struct concurrent_pointer_set {
     pthread_mutex_t mutex;
     struct pointer_set set;
 };
-
-typedef _Atomic(int) refcount_t;
 
 #define QUEUE_DEFAULT_MAX_SIZE 64
 
@@ -274,19 +275,6 @@ static inline void *memdup(const void *restrict src, const size_t n) {
     return memcpy(dest, src, n);
 }
 
-#define BMAP_ELEMENT_TYPE uint8_t
-#define BMAP_ELEMENT_SIZE (sizeof(BMAP_ELEMENT_TYPE))
-#define BMAP_ELEMENT_BITS (BMAP_ELEMENT_SIZE * 8)
-#define BMAP_DECLARATION(name, n_bits) BMAP_ELEMENT_TYPE name[(((n_bits) -1) / BMAP_ELEMENT_BITS) + 1]
-#define BMAP_IS_SET(p_bmap, i_bit) ((p_bmap)[(i_bit) / BMAP_ELEMENT_BITS] & (1 << ((i_bit) & (BMAP_ELEMENT_BITS - 1))))
-#define BMAP_SET(p_bmap, i_bit) ((p_bmap)[(i_bit) / BMAP_ELEMENT_BITS] |= (1 << ((i_bit) & (BMAP_ELEMENT_BITS - 1))))
-#define BMAP_CLEAR(p_bmap, i_bit) ((p_bmap)[(i_bit) / BMAP_ELEMENT_BITS] &= ~(1 << ((i_bit) & (BMAP_ELEMENT_BITS - 1))))
-#define BMAP_ZERO(p_bmap) memset((p_bmap), 0, sizeof(p_bmap) / sizeof(*(p_bmap)))
-#define BMAP_SIZE(p_bmap) (ARRAY_SIZE(p_bmap) * BMAP_ELEMENT_BITS)
-
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
-
 /**
  * @brief Get the current time of the system monotonic clock.
  * @returns time in nanoseconds.
@@ -348,106 +336,6 @@ static inline uint64_t get_monotonic_time(void) {
 
 #define COMPILE_ASSERT_MSG(expression, msg) _Static_assert(expression, msg)
 #define COMPILE_ASSERT(expression) COMPILE_ASSERT_MSG(expression, "Expression evaluates to false")
-
-static inline int refcount_inc_n(refcount_t *refcount, int n) {
-    return atomic_fetch_add_explicit(refcount, n, memory_order_relaxed);
-}
-
-/// Increments the reference count and returns the previous value.
-static inline int refcount_inc(refcount_t *refcount) {
-    return refcount_inc_n(refcount, 1);
-}
-
-/// Decrement the reference count, return true if the refcount afterwards
-/// is still non-zero.
-static inline bool refcount_dec(refcount_t *refcount) {
-    return atomic_fetch_sub_explicit(refcount, 1, memory_order_acq_rel) != 1;
-}
-
-/// Returns true if the reference count is one.
-/// If this is the case you that means this thread has exclusive access
-/// to the object.
-static inline bool refcount_is_one(refcount_t *refcount) {
-    return atomic_load_explicit(refcount, memory_order_acquire) == 1;
-}
-
-/// Returns true if the reference count is zero. Should never be true
-/// in practice because that'd only be the case for a destroyed object.
-/// So this is only really useful for debugging.
-static inline bool refcount_is_zero(refcount_t *refcount) {
-    return atomic_load_explicit(refcount, memory_order_acquire) == 0;
-}
-
-/// Get the current reference count, without any memory ordering restrictions.
-/// Not strictly correct, should only be used for debugging.
-static inline int refcount_get_for_debug(refcount_t *refcount) {
-    return atomic_load_explicit(refcount, memory_order_relaxed);
-}
-
-#define REFCOUNT_INIT_0 (0)
-#define REFCOUNT_INIT_1 (1)
-#define REFCOUNT_INIT_N(n) (n)
-
-#define DECLARE_REF_OPS(obj_name)                                                   \
-    UNUSED struct obj_name *obj_name##_ref(struct obj_name *obj);                   \
-    UNUSED void obj_name##_unref(struct obj_name *obj);                             \
-    UNUSED void obj_name##_unrefp(struct obj_name **obj);                           \
-    UNUSED void obj_name##_swap_ptrs(struct obj_name **objp, struct obj_name *obj); \
-    UNUSED void obj_name##_unref_void(void *obj);
-
-#define DEFINE_REF_OPS(obj_name, refcount_member_name)                               \
-    UNUSED struct obj_name *obj_name##_ref(struct obj_name *obj) {                   \
-        refcount_inc(&obj->refcount_member_name);                                    \
-        return obj;                                                                  \
-    }                                                                                \
-    UNUSED void obj_name##_unref(struct obj_name *obj) {                             \
-        if (refcount_dec(&obj->refcount_member_name) == false) {                     \
-            obj_name##_destroy(obj);                                                 \
-        }                                                                            \
-    }                                                                                \
-    UNUSED void obj_name##_unrefp(struct obj_name **obj) {                           \
-        obj_name##_unref(*obj);                                                      \
-        *obj = NULL;                                                                 \
-    }                                                                                \
-    UNUSED void obj_name##_swap_ptrs(struct obj_name **objp, struct obj_name *obj) { \
-        if (obj != NULL) {                                                           \
-            obj_name##_ref(obj);                                                     \
-        }                                                                            \
-        if (*objp != NULL) {                                                         \
-            obj_name##_unrefp(objp);                                                 \
-        }                                                                            \
-        *objp = obj;                                                                 \
-    }                                                                                \
-    UNUSED void obj_name##_unref_void(void *obj) {                                   \
-        obj_name##_unref((struct obj_name *) obj);                                   \
-    }
-
-#define DEFINE_STATIC_REF_OPS(obj_name, refcount_member_name)                               \
-    UNUSED static struct obj_name *obj_name##_ref(struct obj_name *obj) {                   \
-        refcount_inc(&obj->refcount_member_name);                                           \
-        return obj;                                                                         \
-    }                                                                                       \
-    UNUSED static void obj_name##_unref(struct obj_name *obj) {                             \
-        if (refcount_dec(&obj->refcount_member_name) == false) {                            \
-            obj_name##_destroy(obj);                                                        \
-        }                                                                                   \
-    }                                                                                       \
-    UNUSED static void obj_name##_unrefp(struct obj_name **obj) {                           \
-        obj_name##_unref(*obj);                                                             \
-        *obj = NULL;                                                                        \
-    }                                                                                       \
-    UNUSED static void obj_name##_swap_ptrs(struct obj_name **objp, struct obj_name *obj) { \
-        if (obj != NULL) {                                                                  \
-            obj_name##_ref(obj);                                                            \
-        }                                                                                   \
-        if (*objp != NULL) {                                                                \
-            obj_name##_unrefp(objp);                                                        \
-        }                                                                                   \
-        *objp = obj;                                                                        \
-    }                                                                                       \
-    UNUSED static void obj_name##_unref_void(void *obj) {                                   \
-        obj_name##_unref((struct obj_name *) obj);                                          \
-    }
 
 #define DECLARE_LOCK_OPS(obj_name)                     \
     UNUSED void obj_name##_lock(struct obj_name *obj); \
@@ -556,240 +444,13 @@ static inline void *uint32_to_ptr(const uint32_t v) {
     return u.ptr;
 }
 
-#define CONTAINER_OF(container_type, field_ptr, field_name)                                 \
-    ({                                                                                      \
-        const typeof(((container_type *) 0)->field_name) *__field_ptr_2 = (field_ptr);      \
-        (container_type *) ((char *) __field_ptr_2 - offsetof(container_type, field_name)); \
-    })
-
 #define MAX_ALIGNMENT (__alignof__(max_align_t))
 #define IS_MAX_ALIGNED(num) ((num) % MAX_ALIGNMENT == 0)
-
-typedef struct {
-    uint8_t bytes[16];
-} uuid_t;
-#define UUID(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15) \
-    ((uuid_t){ .bytes = { _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15 } })
-#define CONST_UUID(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15) \
-    ((const uuid_t){ .bytes = { _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15 } })
-
-static inline bool uuid_equals(const uuid_t a, const uuid_t b) {
-    return memcmp(&a, &b, sizeof(uuid_t)) == 0;
-}
-
-static inline void uuid_copy(uuid_t *dst, const uuid_t src) {
-    memcpy(dst, &src, sizeof(uuid_t));
-}
 
 #define DOUBLE_TO_FP1616(v) ((uint32_t) ((v) *65536))
 #define DOUBLE_TO_FP1616_ROUNDED(v) (((uint32_t) (v)) << 16)
 
 typedef void (*void_callback_t)(void *userdata);
-
-/**
- * @brief A 2-dimensional vector with 2 float coordinates.
- *
- */
-struct vec2f {
-    double x, y;
-};
-
-#define VEC2F(_x, _y) ((struct vec2f){ .x = (_x), .y = (_y) })
-
-ATTR_CONST static inline struct vec2f vec2f_add(struct vec2f a, struct vec2f b) {
-    return VEC2F(a.x + b.x, a.y + b.y);
-}
-
-struct vec2i {
-    int x, y;
-};
-
-#define VEC2I(_x, _y) ((struct vec2i){ .x = (_x), .y = (_y) })
-
-/**
- * @brief A quadrilateral with 4 2-dimensional float coordinates.
- *
- */
-struct quad {
-    struct vec2f top_left, top_right, bottom_left, bottom_right;
-};
-
-#define QUAD(_top_left, _top_right, _bottom_left, _bottom_right) \
-    ((struct quad){ .top_left = _top_left, .top_right = _top_right, .bottom_left = _bottom_left, .bottom_right = _bottom_right })
-#define QUAD_FROM_COORDS(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4) QUAD(VEC2F(_x1, _y1), VEC2F(_x2, _y2), VEC2F(_x3, _y3), VEC2F(_x4, _y4))
-
-struct aa_rect {
-    struct vec2f offset, size;
-};
-
-#define AA_RECT(_offset, _size) ((struct aa_rect){ .offset = offset, .size = size })
-#define AA_RECT_FROM_COORDS(offset_x, offset_y, width, height) \
-    ((struct aa_rect){ .offset = VEC2F(offset_x, offset_y), .size = VEC2F(width, height) })
-
-ATTR_CONST static inline struct aa_rect get_aa_bounding_rect(const struct quad _rect) {
-    double l = min(min(min(_rect.top_left.x, _rect.top_right.x), _rect.bottom_left.x), _rect.bottom_right.x);
-    double r = max(max(max(_rect.top_left.x, _rect.top_right.x), _rect.bottom_left.x), _rect.bottom_right.x);
-    double t = min(min(min(_rect.top_left.y, _rect.top_right.y), _rect.bottom_left.y), _rect.bottom_right.y);
-    double b = max(max(max(_rect.top_left.y, _rect.top_right.y), _rect.bottom_left.y), _rect.bottom_right.y);
-    return AA_RECT_FROM_COORDS(l, t, r - l, b - t);
-}
-
-ATTR_CONST static inline struct quad get_quad(const struct aa_rect rect) {
-    return (struct quad){ .top_left = rect.offset,
-                          .top_right.x = rect.offset.x + rect.size.x,
-                          .top_right.y = rect.offset.y,
-                          .bottom_left.x = rect.offset.x,
-                          .bottom_left.y = rect.offset.y + rect.size.y,
-                          .bottom_right.x = rect.offset.x + rect.size.x,
-                          .bottom_right.y = rect.offset.y + rect.size.y };
-}
-
-struct mat3f {
-    double scaleX;
-    double skewX;
-    double transX;
-    double skewY;
-    double scaleY;
-    double transY;
-    double pers0;
-    double pers1;
-    double pers2;
-};
-
-#define FLUTTER_TRANSFORM_AS_MAT3F(_t) \
-    ((struct mat3f){ (_t).scaleX, (_t).skewX, (_t).transX, (_t).skewY, (_t).scaleY, (_t).transY, (_t).pers0, (_t).pers1, (_t).pers2 })
-#define MAT3F_AS_FLUTTER_TRANSFORM(_t) \
-    ((FlutterTransformation            \
-    ){ (_t).scaleX, (_t).skewX, (_t).transX, (_t).skewY, (_t).scaleY, (_t).transY, (_t).pers0, (_t).pers1, (_t).pers2 })
-
-#define MAT3F_TRANSLATION(translate_x, translate_y)                                                                                         \
-    ((struct mat3f                                                                                                                          \
-    ){ .scaleX = 1, .skewX = 0, .transX = translate_x, .skewY = 0, .scaleY = 1, .transY = translate_y, .pers0 = 0, .pers1 = 0, .pers2 = 1 } \
-    )
-
-/**
- * @brief A flutter transformation that rotates any coords around the x-axis, counter-clockwise.
- */
-#define MAT3F_ROTX(deg)                                               \
-    ((struct mat3f){ .scaleX = 1,                                     \
-                     .skewX = 0,                                      \
-                     .transX = 0,                                     \
-                     .skewY = 0,                                      \
-                     .scaleY = cos(((double) (deg)) / 180.0 * M_PI),  \
-                     .transY = -sin(((double) (deg)) / 180.0 * M_PI), \
-                     .pers0 = 0,                                      \
-                     .pers1 = sin(((double) (deg)) / 180.0 * M_PI),   \
-                     .pers2 = cos(((double) (deg)) / 180.0 * M_PI) })
-
-/**
- * @brief A flutter transformation that rotates any coords around the y-axis, counter-clockwise.
- */
-#define MAT3F_ROTY(deg)                                              \
-    ((struct mat3f){ .scaleX = cos(((double) (deg)) / 180.0 * M_PI), \
-                     .skewX = 0,                                     \
-                     .transX = sin(((double) (deg)) / 180.0 * M_PI), \
-                     .skewY = 0,                                     \
-                     .scaleY = 1,                                    \
-                     .transY = 0,                                    \
-                     .pers0 = -sin(((double) (deg)) / 180.0 * M_PI), \
-                     .pers1 = 0,                                     \
-                     .pers2 = cos(((double) (deg)) / 180.0 * M_PI) })
-
-/**
- * @brief A flutter transformation that rotates any coords around the z-axis, counter-clockwise.
- */
-#define MAT3F_ROTZ(deg)                                              \
-    ((struct mat3f){ .scaleX = cos(((double) (deg)) / 180.0 * M_PI), \
-                     .skewX = -sin(((double) (deg)) / 180.0 * M_PI), \
-                     .transX = 0,                                    \
-                     .skewY = sin(((double) (deg)) / 180.0 * M_PI),  \
-                     .scaleY = cos(((double) (deg)) / 180.0 * M_PI), \
-                     .transY = 0,                                    \
-                     .pers0 = 0,                                     \
-                     .pers1 = 0,                                     \
-                     .pers2 = 1 })
-
-/**
- * @brief Returns a matrix that is the result of matrix-multiplying a with b.
- *
- * @param a The first (lhs) input matrix.
- * @param b The second (rhs) input matrix.
- * @return struct mat3f The product of a x b.
- */
-ATTR_CONST static inline struct mat3f multiply_mat3f(const struct mat3f a, const struct mat3f b) {
-    return (struct mat3f){ .scaleX = a.scaleX * b.scaleX + a.skewX * b.skewY + a.transX * b.pers0,
-                           .skewX = a.scaleX * b.skewX + a.skewX * b.scaleY + a.transX * b.pers1,
-                           .transX = a.scaleX * b.transX + a.skewX * b.transY + a.transX * b.pers2,
-                           .skewY = a.skewY * b.scaleX + a.scaleY * b.skewY + a.transY * b.pers0,
-                           .scaleY = a.skewY * b.skewX + a.scaleY * b.scaleY + a.transY * b.pers1,
-                           .transY = a.skewY * b.transX + a.scaleY * b.transY + a.transY * b.pers2,
-                           .pers0 = a.pers0 * b.scaleX + a.pers1 * b.skewY + a.pers2 * b.pers0,
-                           .pers1 = a.pers0 * b.skewX + a.pers1 * b.scaleY + a.pers2 * b.pers1,
-                           .pers2 = a.pers0 * b.transX + a.pers1 * b.transY + a.pers2 * b.pers2 };
-}
-
-/**
- * @brief Returns a matrix that is the result of element-wise addition of a and b.
- *
- * @param a The lhs input matrix.
- * @param b The rhs input matrix.
- * @return struct mat3f The result of a + b. (element-wise)
- */
-ATTR_CONST static inline struct mat3f add_mat3f(const struct mat3f a, const struct mat3f b) {
-    return (struct mat3f){ .scaleX = a.scaleX + b.scaleX,
-                           .skewX = a.skewX + b.skewX,
-                           .transX = a.transX + b.transX,
-                           .skewY = a.skewY + b.skewY,
-                           .scaleY = a.scaleY + b.scaleY,
-                           .transY = a.transY + b.transY,
-                           .pers0 = a.pers0 + b.pers0,
-                           .pers1 = a.pers1 + b.pers1,
-                           .pers2 = a.pers2 + b.pers2 };
-}
-
-/**
- * @brief Returns the transponated of a.
- *
- * @param a The input matrix.
- * @return struct mat3f a transponated.
- */
-ATTR_CONST static inline struct mat3f transponate_mat3f(const struct mat3f a) {
-    return (struct mat3f){
-        .scaleX = a.scaleX,
-        .skewX = a.skewY,
-        .transX = a.pers0,
-        .skewY = a.skewX,
-        .scaleY = a.scaleY,
-        .transY = a.pers1,
-        .pers0 = a.transX,
-        .pers1 = a.transY,
-        .pers2 = a.pers2,
-    };
-}
-
-ATTR_CONST static inline struct vec2f transform_point(const struct mat3f transform, const struct vec2f point) {
-    return VEC2F(
-        transform.scaleX * point.x + transform.skewX * point.y + transform.transX,
-        transform.skewY * point.x + transform.scaleY * point.y + transform.transY
-    );
-}
-
-ATTR_CONST static inline struct quad transform_quad(const struct mat3f transform, const struct quad rect) {
-    return QUAD(
-        transform_point(transform, rect.top_left),
-        transform_point(transform, rect.top_right),
-        transform_point(transform, rect.bottom_left),
-        transform_point(transform, rect.bottom_right)
-    );
-}
-
-ATTR_CONST static inline struct quad transform_aa_rect(const struct mat3f transform, const struct aa_rect rect) {
-    return transform_quad(transform, get_quad(rect));
-}
-
-ATTR_CONST static inline struct vec2f vec2f_swap_xy(const struct vec2f point) {
-    return VEC2F(point.y, point.x);
-}
 
 ATTR_PURE static inline bool streq(const char *a, const char *b) {
     return strcmp(a, b) == 0;
