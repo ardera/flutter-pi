7b40824c9957cf2b1598233ae06b4edd8ef169ce
Hannes Winkler <hanneswinkler2000@web.de>
2023-06-13 16:29:01 +0200

    new mouse cursor icons - use pointer icons from Android - add pointer icons for different pointer kinds   (currently basic (arrow), click and text) - compress cursor icons using RLE - add new pointer_icon_* API for querying and using the pointer icons - remove old pointer_icon API

--- src/cursor.h
+++ src/cursor.h
@@ -3,14 +3,59 @@
 
 #include <stdint.h>
 
-struct cursor_icon {
-    int rotation;
-    int hot_x, hot_y;
-    int width, height;
-    uint32_t *data;
+#include "util/geometry.h"
+
+enum pointer_kind {
+    POINTER_KIND_NONE,
+    POINTER_KIND_BASIC,
+    POINTER_KIND_CLICK,
+    POINTER_KIND_FORBIDDEN,
+    POINTER_KIND_WAIT,
+    POINTER_KIND_PROGRESS,
+    POINTER_KIND_CONTEXT_MENU,
+    POINTER_KIND_HELP,
+    POINTER_KIND_TEXT,
+    POINTER_KIND_VERTICAL_TEXT,
+    POINTER_KIND_CELL,
+    POINTER_KIND_PRECISE,
+    POINTER_KIND_MOVE,
+    POINTER_KIND_GRAB,
+    POINTER_KIND_GRABBING,
+    POINTER_KIND_NO_DROP,
+    POINTER_KIND_ALIAS,
+    POINTER_KIND_COPY,
+    POINTER_KIND_DISAPPEARING,
+    POINTER_KIND_ALL_SCROLL,
+    POINTER_KIND_RESIZE_LEFT_RIGHT,
+    POINTER_KIND_RESIZE_UP_DOWN,
+    POINTER_KIND_RESIZE_UP_LEFT_DOWN_RIGHT,
+    POINTER_KIND_RESIZE_UP_RIGHT_DOWN_LEFT,
+    POINTER_KIND_RESIZE_UP,
+    POINTER_KIND_RESIZE_DOWN,
+    POINTER_KIND_RESIZE_LEFT,
+    POINTER_KIND_RESIZE_RIGHT,
+    POINTER_KIND_RESIZE_UP_LEFT,
+    POINTER_KIND_RESIZE_UP_RIGHT,
+    POINTER_KIND_RESIZE_DOWN_LEFT,
+    POINTER_KIND_RESIZE_DOWN_RIGHT,
+    POINTER_KIND_RESIZE_COLUMN,
+    POINTER_KIND_RESIZE_ROW,
+    POINTER_KIND_ZOOM_IN,
+    POINTER_KIND_ZOOM_OUT
 };
 
-extern const struct cursor_icon cursors[5];
-extern int n_cursors;
+struct pointer_icon;
+
+const struct pointer_icon *pointer_icon_for_details(enum pointer_kind kind, double pixel_ratio);
+
+enum pointer_kind pointer_icon_get_kind(const struct pointer_icon *icon);
+
+float pointer_icon_get_pixel_ratio(const struct pointer_icon *icon);
+
+struct vec2i pointer_icon_get_size(const struct pointer_icon *icon);
+
+struct vec2i pointer_icon_get_hotspot(const struct pointer_icon *icon);
+
+void *pointer_icon_dup_pixels(const struct pointer_icon *icon);
 
 #endif
