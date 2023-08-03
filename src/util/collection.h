891c0c0343ec3a646e5d1ed410fab961a610de3f
Hannes Winkler <hanneswinkler2000@web.de>
2023-08-03 21:23:40 +0200

    util: move lock ops into separate file

--- src/util/collection.h
+++ src/util/collection.h
@@ -23,6 +23,7 @@
 #include "macros.h"
 #include "refcounting.h"
 #include "uuid.h"
+#include "lock_ops.h"
 
 struct queue {
     size_t start_index;
@@ -286,52 +287,6 @@ static inline uint64_t get_monotonic_time(void) {
     clock_gettime(CLOCK_MONOTONIC, &time);
     return time.tv_nsec + time.tv_sec * 1000000000ull;
 }
-
-#define DECLARE_LOCK_OPS(obj_name)                     \
-    UNUSED void obj_name##_lock(struct obj_name *obj); \
-    UNUSED void obj_name##_unlock(struct obj_name *obj);
-
-#define DEFINE_LOCK_OPS(obj_name, mutex_member_name)        \
-    UNUSED void obj_name##_lock(struct obj_name *obj) {     \
-        int ok;                                             \
-        ok = pthread_mutex_lock(&obj->mutex_member_name);   \
-        ASSERT_EQUALS_MSG(ok, 0, "Error locking mutex.");   \
-        (void) ok;                                          \
-    }                                                       \
-    UNUSED void obj_name##_unlock(struct obj_name *obj) {   \
-        int ok;                                             \
-        ok = pthread_mutex_unlock(&obj->mutex_member_name); \
-        ASSERT_EQUALS_MSG(ok, 0, "Error unlocking mutex."); \
-        (void) ok;                                          \
-    }
-
-#define DEFINE_STATIC_LOCK_OPS(obj_name, mutex_member_name)      \
-    UNUSED static void obj_name##_lock(struct obj_name *obj) {   \
-        int ok;                                                  \
-        ok = pthread_mutex_lock(&obj->mutex_member_name);        \
-        ASSERT_EQUALS_MSG(ok, 0, "Error locking mutex.");        \
-        (void) ok;                                               \
-    }                                                            \
-    UNUSED static void obj_name##_unlock(struct obj_name *obj) { \
-        int ok;                                                  \
-        ok = pthread_mutex_unlock(&obj->mutex_member_name);      \
-        ASSERT_EQUALS_MSG(ok, 0, "Error unlocking mutex.");      \
-        (void) ok;                                               \
-    }
-
-#define DEFINE_INLINE_LOCK_OPS(obj_name, mutex_member_name)             \
-    UNUSED static inline void obj_name##_lock(struct obj_name *obj) {   \
-        int ok;                                                         \
-        ok = pthread_mutex_lock(&obj->mutex_member_name);               \
-        ASSERT_EQUALS_MSG(ok, 0, "Error locking mutex.");               \
-        (void) ok;                                                      \
-    }                                                                   \
-    UNUSED static inline void obj_name##_unlock(struct obj_name *obj) { \
-        int ok;                                                         \
-        ok = pthread_mutex_unlock(&obj->mutex_member_name);             \
-        ASSERT_EQUALS_MSG(ok, 0, "Error unlocking mutex.");             \
-        (void) ok;                                                      \
-    }
 
 #define BITCAST(to_type, value) (*((const to_type *) (&(value))))
 
