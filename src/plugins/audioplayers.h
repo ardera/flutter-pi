692cec24763c83c880820a7205cb5c7c25eb6b5f
Douman <douman@gmx.se>
2024-01-15 22:05:13 +0900

    Update to audioplayers v5 (#345)
    * Update to audioplayers v5
    * Update audioplayers impl:
    
    Emit error if set source fails to set ready state
    Implement new release method
    Return NULL in align with audioplayers API
    Fix playbin identification in on media state change
    
    * Update note on supported version of audioplayers

--- src/plugins/audioplayers.h
+++ src/plugins/audioplayers.h
@@ -30,10 +30,23 @@ void audio_player_set_volume(struct audio_player *self, double volume);
 
 void audio_player_set_playback_rate(struct audio_player *self, double rate);
 
+void audio_player_set_balance(struct audio_player *self, double balance);
+
 void audio_player_set_position(struct audio_player *self, int64_t position);
 
 void audio_player_set_source_url(struct audio_player *self, char *url);
 
 bool audio_player_is_id(struct audio_player *self, char *id);
+
+const char* audio_player_subscribe_channel_name(const struct audio_player *self);
+
+///Asks to subscribe to channel events
+///
+///`value` - Indicates whether to subscribe or unsubscribe
+///
+///Returns `true` if player uses `channel`, otherwise returns `false
+bool audio_player_set_subscription_status(struct audio_player *self, const char *channel, bool value);
+
+void audio_player_release(struct audio_player *self);
 
 #endif  // AUDIOPLAYERS_H_
