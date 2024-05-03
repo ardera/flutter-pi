641b8014c8898a586424a7c279982c8680088e29
Hannes Winkler <hanneswinkler2000@web.de>
2024-05-03 13:47:32 +0000

    sentry plugin: bundle crashpad_handler

--- src/main.c
+++ src/main.c
@@ -1,5 +1,30 @@
+
+#include <stdbool.h>
+#include <string.h>
+
+#include "config.h"
+
 int flutterpi_app_main(int argc, char **argv);
+int crashpad_handler_main(int argc, char **argv);
+
+#ifdef HAVE_BUNDLED_CRASHPAD_HANDLER
+static bool running_in_crashpad_mode(int argc, char **argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--attachment=FlutterpiCrashpadHandlerMode") == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+#endif
 
 int main(int argc, char **argv) {
+#ifdef HAVE_BUNDLED_CRASHPAD_HANDLER
+    if (running_in_crashpad_mode(argc, argv)) {
+        return crashpad_handler_main(argc, argv);
+    }
+#endif
+
     return flutterpi_app_main(argc, argv);
 }
